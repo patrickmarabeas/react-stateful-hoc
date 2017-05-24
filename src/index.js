200a86ee564d82e3ae2399cc86e987bd80169b1b
Patrick Marabeas <patrick.marabeas@heathwallace.com>
2017-05-24 11:19:17 +1000

    Add licence; bump version 0.1.2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+/**
+ * react-stateful-hoc v0.1.2
+ * https://github.com/patrickmarabeas/react-stateful-hoc
+ *
+ * Copyright 2017, Patrick Marabeas http://marabeas.io
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Date: 24/05/17
+ */
+
 import React from 'react';
 
 export default StatelessComponent => (initialState = {}) => class extends React.Component {
